333f88c31a2aa133282904c40303501170fb3ae4
WaffleTheCool <wilmer7889@gmail.com>
2022-12-24 13:13:08 +0100

    Another fix

--- template/createQMOD.ps1
+++ template/createQMOD.ps1
@@ -2,7 +2,7 @@
 cargo ndk build --release
 
 if ($?) {
-    Compress-Archive -Path "./mod.json", "./target/aarch64-linux-android/release/libpinkcute.so" -DestinationPath "./pink_cute_v0.1.1.zip" -Update
-    Remove-Item "./pink_cute_v0.1.1.qmod"
-    Rename-Item "./pink_cute_v0.1.1.zip" "./pink_cute_v0.1.1.qmod"
+    Compress-Archive -Path "./mod.json", "./target/aarch64-linux-android/release/lib#{ID}.so" -DestinationPath "./#{ID}_${version}.zip" -Update
+    Remove-Item "./#{ID}_${version}.qmod"
+    Rename-Item "./#{ID}_${version}.zip" "./#{ID}_${version}.qmod"
 }
